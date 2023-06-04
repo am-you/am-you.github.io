0c823cd4efbc95b924320fda4b62dd3f4254c0d2
am-you <52196250+am-you@users.noreply.github.com>
2023-06-04 17:59:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const list=document.querySelector(".list");
-const grid=document.querySelector(".grid");
-const ltg=document.querySelector(".ltg");
-list.addEventListener("click", () => {
-	ltg.classList.add("list")
-})
-grid.addEventListener("click", () => {
-	ltg.classList.remove("list");
-})
+let videoList = document.querySelectorAll('.video-list-container .list');
+
+videoList.forEach(vid =>{
+   vid.onclick = () =>{
+      videoList.forEach(remove =>{remove.classList.remove('active')});
+      vid.classList.add('active');
+      let src = vid.querySelector('.list-video').src;
+      let title = vid.querySelector('.list-title').innerHTML;
+      document.querySelector('.main-video-container .main-video').src = src;
+      document.querySelector('.main-video-container .main-video').play();
+      document.querySelector('.main-video-container .main-vid-title').innerHTML = title;
+   };
+});
